f88793176103171ffd74083c4264d1b58e4499da
Niruthiya Narashiman Srinivasan <powerofniruthiya@gmail.com>
2024-09-20 22:52:57 -0400

    use third party api for job description
    Used Third party api from rapid api
    https://rapidapi.com/rockapis-rockapis-default/api/linkedin-api8
    To scrape job description form

--- jobDescription.py
+++ jobDescription.py
@@ -1,49 +1,17 @@
-description = '''About the job
-Who We Are
-We are a Plymouth, Michigan-based startup, developing cutting-edge technologies for Driver Assistance and Automated Driving systems using artificial intelligence, advanced controls techniques, utilizing high-end CPU/GPU and IoT technologies. We have decades of automotive development experience and expertise to build mature automotive grade products. If you're interested in getting in on the ground-floor of an opportunity that will revolutionize the trucking industry, this is it.
+import requests
+import re
 
+def job_details_provider(job_posting_url):
 
-Who You Are
-You are innovative, love challenging problems and building products from the ground up. You want to work on cutting-edge technologies that make a difference. You want to see your work have a direct impact on the company's product. You communicate, you teach, you learn, and you love working in a diverse team.
+    url = "https://linkedin-api8.p.rapidapi.com/get-job-details"
 
+    job_id = re.search(r'currentJobId=(\d+)', job_posting_url).group(1)
+    
+    headers = {
+	"x-rapidapi-key": "f8f9a6a8d1mshe2f417cbda2e8a2p14afcbjsn71209cf60d0b",
+	"x-rapidapi-host": "linkedin-api8.p.rapidapi.com"
+    }
 
+    response = requests.get(url, headers=headers, params={"id":job_id}).json()
+    return (response['data']['company']['universalName'],response['data']['title'],response['data']['description'])
 
-
-Role Description 
-
-Developing software layers for communication over SPI, I2C, CAN and Ethernet for variety of sensor and system interfaces 
-Develop calibration interface and programming over CAN 
-Develop IoT and Over-the-Air programming software layers 
-Develop PC-based test and development tools in various environments such as LabVIEW, Python, C, C++, etc 
-Bus load simulation with CAN / Ethernet network with UDP, TCP packet generation and analysis of bandwidth 
-Deploying Matlab/Simulink auto-generated code on embedded target CPU/GPUs 
-Unit and Integration testing and validation of embedded systems 
-Support for demo vehicle integration and testing activities 
-Simulation and testing of CAN/J1939 Networks 
-
-
-Minimum Qualifications and Skills 
-
-Bachelor's degree in Computer Science, Electrical, Electronics or Computer Engineering with a strong software focus 
-Minimum of five years of progressive engineering experience in automotive embedded software development or proven accomplishments otherwise 
-Mastery of C, C++ 
-Mastery of CAN tools such as CANalyzer, CANape 
-Multithreading, multiprocessing, POSIX threads, Embedded Linux, Real-time systems 
-Ability to understand schematic design and drawings. 
-Self-starter with the ability to work as part of a team in a fast-paced startup environment. 
- 
-Preferred Qualifications and Skills 
-
-Master’s degree or PhD in Electrical, Computer or Software Engineering 
-Strong competency Python, Matlab/Simulink and LabVIEW 
-Five years of minimum developing embedded software for automotive systems 
-High safety integrity software development experience. ISO26262 and SAE/ISO 21434 experience 
-Vehicle diagnostics design & development experience 
-Highly skilled GitHub and/or other similar SCM tools 
-
-
-
-
-Traxen is an Equal Employment Opportunity employer that proudly pursues and hires a diverse workforce. Traxen does not make hiring or employment decisions on the basis of race, color, religion or religious belief, ethnic or national origin, nationality, sex, gender, gender-identity, sexual orientation, disability, age, military or veteran status, or any other basis protected by applicable local, state, or federal laws or prohibited by Company policy. Traxen also strives for a healthy and safe workplace and strictly prohibits harassment of any kind.
-
-'''
